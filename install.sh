b8adcd72119c008de09d519529635106c4fa8871
Manish Vishnoi <manish.vishnoi2@gmail.com>
2019-04-25 19:00:45 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,25 +5,3 @@ pip3 install --user keras=2.0.0
 pip3 install --user sklearn
 pip3 install --user matplotlib
 pip3 install --user nltk
-
-PS3='Please enter your choice to run : '
-options=("Supervised learning" "Neural Network : LSTM" "Unsupervised Learning" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "Supervised learning")
-            echo "you chose choice 1"
-            ;;
-        "Neural Network : LSTM")
-            echo "you chose Neural Network : LSTM"
-            python3 "code/Neural Network/lstm_prediction.py"
-            ;;
-        "Unsupervised Learning")
-            echo "you chose Unsupervised Learning"
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
-    esac
-done
